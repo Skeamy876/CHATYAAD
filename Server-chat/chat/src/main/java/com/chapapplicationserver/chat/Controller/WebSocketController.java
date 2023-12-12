c5a693cf40e6b95c27537ff0e0797aefc84c1269
Skeamy876 <awinstonmclennon@gmail.com>
2023-12-11 21:26:42 -0500

    testing

--- Server-chat/chat/src/main/java/com/chapapplicationserver/chat/Controller/WebSocketController.java
+++ Server-chat/chat/src/main/java/com/chapapplicationserver/chat/Controller/WebSocketController.java
@@ -3,26 +3,31 @@ package com.chapapplicationserver.chat.Controller;
 import com.chapapplicationserver.chat.Collection.ChatMessage;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
+
+import java.util.Objects;
 
 @Controller
 public class WebSocketController {
-
+    private final Logger log = LoggerFactory.getLogger(WebSocketController.class);
     @MessageMapping("/chat.register")
     @SendTo("/chatRoom/public")
     public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
+        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username",chatMessage.getSender());
+        log.info("Message that client has joined: {}",chatMessage);
         return chatMessage;
     }
 
     @MessageMapping("/chat.send")
     @SendTo("/chatRoom/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        log.info("Message sent from client to public chat: {}",chatMessage);
         return chatMessage;
     }
 
--- client-sidechat-V1/src/pages/Chat.js
+++ client-sidechat-V1/src/pages/Chat.js
@@ -5,7 +5,7 @@ import {over} from 'stompjs';
 
 let stompClient = null;
 const Chat = () => {   
-    const testUrl = 'http://localhost:8080/connectpoint/'
+    const testUrl = 'http://localhost:8080/'
     const serverurl= 'http://chatspringapp-env-2.eba-hyskc2i5.us-east-1.elasticbeanstalk.com/'
     const[User, setUser] = useState({
         username:" ",
@@ -29,6 +29,7 @@ const Chat = () => {
     });
     const [Users,setUsers] = useState([])
     const messageListRef = useRef(null);
+    const [inputValue, setInputValue] = useState('');
 
 
     useEffect(()=>{
@@ -40,7 +41,7 @@ const Chat = () => {
     },[]);
 
     const connect=()=>{
-        const socket = new SockJS(testUrl);
+        const socket = new SockJS(`${testUrl}connectpoint/`);
         stompClient = over(socket);
         stompClient.connect({},function(frame){
             stompClient.debug("Connected: " + frame);
@@ -61,6 +62,8 @@ const Chat = () => {
     const onSendChatMessage=()=>{
         if (stompClient) {
             stompClient.send("chatapp/chat.send", {}, JSON.stringify(sendMessage));
+            setSendMessage(prevMessage => ({ ...prevMessage, content: '' }));
+            setInputValue('');
         } 
     }
 
@@ -71,19 +74,19 @@ const Chat = () => {
 
     const onMessageRecieved=(payload)=>{
         let message = JSON.parse(payload.body);
+        console.log("This is onMessageRecieved")
+        console.log(message);
         chatMessages.push(message);
         setChatMessages([...chatMessages]);
-        console.log(chatMessages)
     };
 
     const handleChange = (e) => {
-        const {value} = e.target; 
-        setSendMessage({
-            content: value,
-            sender: User.username,
+        setInputValue(e.target.value);
+        setSendMessage(prevMessage => ({ ...prevMessage, 
+            content: e.target.value,
             timestamp: getTime(),
-            type: 'CHAT'
-        });
+            type: "CHAT", 
+        }));
     };
     const handleKeydown= (e) =>{
         if(e.key ==='Enter'){
@@ -97,78 +100,107 @@ const Chat = () => {
     };
 
 
-    const getRandomColor = () => {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
+    const avatars = [
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp",
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp",
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3-bg.webp",
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava4-bg.webp",
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava5-bg.webp",
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp",
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava7-bg.webp",
+        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava8-bg.webp",
+    ];
+
+    const getRandomAvatar = () => {
+        return avatars[Math.floor(Math.random() * avatars.length)];
     };
 
     let JoinMessages = chatMessages.filter(message=>message.type ==="JOIN");
     let AllChatMessages = chatMessages.filter(message => message.type ==="CHAT");
-    let colorForCurrentUser = getRandomColor();
 
-
-    console.log(chatMessages)
-
-        
-    
-        
-            
         
     return (
     <>
         <div>
-            {!User.conected ? 
+        {!User.conected ? 
             <div className="container text-center mt-5">
                 <h1>Welcome to the ChatYaad</h1>
                 <p>Select a chat room from the dropdown, then click on the button below to connect to the Chat Room</p>
                 <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
                     <option defaultValue={" "}>Select a room...</option>
                     <option value="1">Room one</option>
-                    {/* Add more options here */}
                 </select>
                 
                 <button className="btn btn-primary btn-lg fixed-bottom" onClick={connect}>Connect</button>
             </div>
             :
-            <div className="area-container">                
-                <div className="chat-container">
-                    <div className="chatRoomHead text-bg-dark">
-                        <h1 className="chatRoom_Header">ChatRoom</h1>
-                        <ul>{Users.map((user,index)=><li key={index} className=" p-1"  style={{display:"inline"}}> {user} </li>)}</ul>
-                    </div>
-                    <div className="chatRoom_Messages" ref={messageListRef}>
-                        <div className="card message-list">
-                            <ul className="list-group list-group-flush">
-                                {JoinMessages.map((message, index)=>(
-                                    <li key={index} className="list-group-item"> {message.sender} has joined the chat</li> 
-                                ))}
-                                {AllChatMessages.map((message, index) => (
-                                    <li key={index} className="list-group-item p-3">
-                                        <div id="author" style={{ color: message.sender === User.username ? colorForCurrentUser : getRandomColor() }}>
-                                            {message.sender}
-                                        </div> :
-                                        {message.content}
-                                    </li>
-                                ))}
-                            </ul>
-                            {scrollToBottom()}
+            <section >
+                <div className="container py-5">
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-md-8 col-lg-6 col-xl-12">
+                            <div className="card" id="chat1" style={{borderRadius: "15px"}}>
+                                <div
+                                    className="card-header d-flex justify-content-between align-items-center p-3 bg-dark text-white border-bottom-0"
+                                    style={{borderTopLeftRadius: '15px', borderTopRightRadius: '15px'}}>
+                                    <i className="fas fa-angle-left"></i>
+                                    <p className="mb-0 fw-bold">Chat Room</p>
+                                    <i className="fas fa-times"></i>
+                                </div>
+                                <div className="card-body">
+                                    {JoinMessages.map((message, index) => {
+                                        return (
+                                            <div className="d-flex flex-row justify-content-start mb-4" key={index}>
+                                                <img src={getRandomAvatar()}
+                                                    alt="avatar 1" style={{width: '45px', height: '100%'}} />
+                                                <div className="p-3 ms-3" style={{borderRadius: '15px', backgroundColor: 'rgba(57, 192, 237,.2)'}}>
+                                                    <p className="small mb-0">{message.sender} joined the chat</p>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+
+                                    {AllChatMessages.map((message, index) => {
+                                        return (
+                                            <div className="d-flex flex-row justify-content-start mb-4" key={index}>
+                                                <img src={getRandomAvatar()}
+                                                    alt="avatar 1" style={{width: '45px', height: '100%'}} />
+                                                    <p>{message.sender}</p>
+                                                <div className="p-3 ms-3" style={{borderRadius: '15px', backgroundColor: 'rgba(57, 192, 237,.2)'}}>
+                                                    <p className="small mb-0">{message.content}</p>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+
+                                    {/* <div className="d-flex flex-row justify-content-end mb-4">
+                                        <div className="p-3 me-3 border" style={{borderRadius: '15px', backgroundColor: '#fbfbfb'}}>
+                                            <p className="small mb-0">Thank you, I really like your product.</p>
+                                        </div>
+                                        <img src={getRandomAvatar()}
+                                            alt="avatar 1" style={{width: '45px', height: '100%'}} />
+                                    </div> */}
+                                    {scrollToBottom()}
+                                
+                                    <div className="form-outline">
+                                        <textarea 
+                                        className="form-control" 
+                                        id="textAreaExample" 
+                                        rows="2" 
+                                        value={inputValue}
+                                        onChange={handleChange}
+                                        onKeyDown={handleKeydown}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="chatRoom_Input">
-                        <input type="text" 
-                            onChange={handleChange}  
-                            onKeyDown={handleKeydown} 
-                            placeholder="Type your message here..."/>
-                        <button type="submit" 
-                        onClick={onSendChatMessage}>Send</button>                  
-                    </div>
                 </div>
-            </div>
-            }
+            </section>
+              
+          
+
+        }
         </div>
     </>
     );
