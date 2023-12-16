5cea22d23927b0efcd00bc8c9d1b9306d1bfe763
Skeamy876 <awinstonmclennon@gmail.com>
2023-12-16 10:32:08 -0500

    re added / to orgins

--- Server-chat/chat/src/main/java/com/chapapplicationserver/chat/Config/SecurityConfiguraition.java
+++ Server-chat/chat/src/main/java/com/chapapplicationserver/chat/Config/SecurityConfiguraition.java
@@ -40,7 +40,7 @@ public class SecurityConfiguraition {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://chatyaad-frontend-production.up.railway.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/","https://chatyaad-frontend-production.up.railway.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
